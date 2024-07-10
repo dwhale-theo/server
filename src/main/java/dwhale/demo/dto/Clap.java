afff0582c13a785c99c4aad985ed54d8bf0f29e5
gobeomjun <theo@dwhale.kr>
2024-07-10 23:43:12 +0900

    link

--- src/main/java/dwhale/demo/dto/Clap.java
+++ src/main/java/dwhale/demo/dto/Clap.java
@@ -8,10 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class Clap {
-
-    private String id;
-    private int price;
-    public String name;
     public String description;
     public String image;
     public String email;
