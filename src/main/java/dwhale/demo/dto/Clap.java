43bcf9d59fbd2531e2bfcf077dfc79faf327dde6
gobeomjun <theo@dwhale.kr>
2024-07-10 15:05:19 +0900

    wewe

---
+++ src/main/java/dwhale/demo/dto/Clap.java
@@ -0,0 +1,16 @@
+package dwhale.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Clap {
+
+    private String id;
+    public String name;
+    public String description;
+    public String image;
+}
